60ab5c668f7a9395d5af198e695ea93f3f726201
pogikeith <pogikeith@yahoo.com>
2019-11-21 13:11:48 -0700

    mediaq

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -17,19 +17,19 @@ class Register extends React.Component {
               Now Enrolling for <br/>
               2019 – 2020 Classes
             </div>
-          <div className = "policies-body">
-          <h2>Accepting Ages 3 - 5</h2>
+          <div className = "regist-body">
+          <h5>Accepting Ages 3 - 5</h5>
           Class Size is limited so please contact me today to start the registration process or<br></br> to put your name on a waiting list
           </div>
 
-          <div className = "policies-body">
-          <h2>There are 2 separate classes being offered</h2>
+          <div className = "regist-body">
+          <h5>There are 2 separate classes being offered</h5>
           Monday and Wednesday Time: 8:30 - 12:00pm Cost: $120 per month<br></br> Tuesday and Thursday Time: 8:30 - 12:00pm. Cost: $120 per month 
           <br></br> Call or email for pricing on Monday - Thursday classes<br></br>
           </div>
 
-          <div className = "policies-body">
-          <h2>General Questions</h2>
+          <div className = "regist-body">
+          <h5>General Questions</h5>
           If you have general questions, would like to schedule a visit, start the registration<br></br> process, or put your child’s name on a waiting 
           list:<br></br> 
           <br></br>Call or Text us at 520-305-9083 or Contact us by e-mail<br></br> 
